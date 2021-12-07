2328a4f7c7967df4a9c4f9722eab524a9314349b
Owen HT Lu <cfleu198@gmail.com>
2021-12-07 21:40:34 +0800

    Add files via upload

--- send_data.py
+++ send_data.py
@@ -7,32 +7,50 @@ from wisepaasdatahubedgesdk.Model.Edge import EdgeAgentOptions, MQTTOptions, DCC
 from wisepaasdatahubedgesdk.Model.Edge import DeviceConfig, AnalogTagConfig, EdgeData, EdgeTag, TextTagConfig
 import wisepaasdatahubedgesdk.Common.Constants as constant
 
+# NodeName: dai
+# NodeID: 444cbfad-250c-4243-a14f-5ea956339702
+# Cre: a56a8afb7d6c9595a1d838a6d95c2dlb
+# DCCS URL: http://api-dccs-ensaas.aiot.twcc.ai/
 
-def scada_conn (scada_id, device_id, device_name):
+SCADA_ID = '444cbfad-250c-4243-a14f-5ea956339702'
+SCADA_NAME = 'kelly'
 
-    # TODO: tobe comfirm
-    # datahub
-    DHB_MQTT_HOST = '10.10.0.14'
-    DHB_MQTT_PORT = 1883
-    DHB_MQTT_USER = "6d657c0a-7fc9-44be-b220-819efb23fbdb:403e3cd7-92ef-457b-9309-64970864bde5"
-    DHB_MQTT_PASS = "u3s9tqk0bvn9sklt6dr8j1jpe9"
-    
+NODE_NAME = 'dai'
+DEVICE_ID = 'm01'
+DEVICE_NAME = 'machine01'
+
+DHB_MQTT_HOST = '103.124.74.162'
+DHB_MQTT_PORT = 1883
+DHB_MQTT_USER = "b8qJj7g4plqa%3A8xen3XV9M7qD:xeCZGRBPLcOLQrUoGHen"
+DHB_MQTT_PASS = "xeCZGRBPLcOLQrUoGHen"
+
+DCCS_KEY = "a56a8afb7d6c9595a1d838a6d95c2dlb"
+DCCS_URL = "http://api-dccs-ensaas.aiot.twcc.ai/"
+
+
+def scada_conn ():
+
     options = EdgeAgentOptions(
-        reconnectInterval = 1, # MQTT reconnect interval seconds
-        nodeId = scada_id, # Getting from SCADA portal
-        deviceId = device_id, # If type is Device, DeviceId must be filled
-        type = constant.EdgeType['Gateway'], # Choice your edge is Gateway or Device, Default is Gateway
-        heartbeat = 60, # Default is 60 seconds
-        dataRecover = False, # Need to recover data or not when disconnected
-        connectType = constant.ConnectType['MQTT'], # Connection type (DCCS, MQTT), default is DCCS
-        
-        MQTT = MQTTOptions( # If connectType is MQTT, must fill this options
-            hostName = DHB_MQTT_HOST,
-            port = DHB_MQTT_PORT,
-            userName = DHB_MQTT_USER,
-            password = DHB_MQTT_PASS,
-            protocalType = constant.Protocol['TCP'] # MQTT protocal (TCP, Websocket), default is TCP
-        )
+        #reconnectInterval = 1, # MQTT reconnect interval seconds
+        #nodeId = DEVICE_ID, # Getting from SCADA portal
+        #deviceId = DEVICE_ID, # If type is Device, DeviceId must be filled
+        #type = constant.EdgeType['Gateway'], # Choice your edge is Gateway or Device, Default is Gateway
+        #heartbeat = 60, # Default is 60 seconds
+        #dataRecover = False, # Need to recover data or not when disconnected
+
+        # connectType = constant.ConnectType['MQTT'], # Connection type (DCCS, MQTT), default is DCCS    
+        # MQTT = MQTTOptions( # If connectType is MQTT, must fill this options
+        #     hostName = DHB_MQTT_HOST,
+        #     port = DHB_MQTT_PORT,
+        #     userName = DHB_MQTT_USER,
+        #     password = DHB_MQTT_PASS,
+        #     protocalType = constant.Protocol['TCP'] # MQTT protocal (TCP, Websocket), default is TCP
+        # )
+
+        connectType = constant.ConnectType['DCCS'],
+        DCCS = DCCSOptions(
+                apiUrl = DCCS_URL, 
+                credentialKey = DCCS_KEY)
     )
 
     edgeAgent = EdgeAgent( options = options )
@@ -42,10 +60,6 @@ def scada_conn (scada_id, device_id, device_name):
 
 def scada_client (data_dict):
 
-    # TODO
-    output_df = pd.DataFrame(list(data_dict.items()),columns = ['features','values']).T
-
-    
     config = EdgeConfig()
 
     # print('scada session #2')
@@ -61,66 +75,51 @@ def scada_client (data_dict):
     config.node = nodeConfig
 
     # print('scada session #3')
-    deviceConfig = DeviceConfig(id = device_id,
-                                name = device_name,
+    deviceConfig = DeviceConfig(id = DEVICE_ID,
+                                name = DEVICE_NAME,
                                 #comPortNumber = None,
                                 deviceType = 'Device Type',
                                 #description = 'w46 Device',
-                                description = device_name
+                                description = DEVICE_NAME
                                 #ip = None,
                                 #port = None
                                 )
     config.node.deviceList.append(deviceConfig)
 
-    # print('scada session #4')
-    for feature in list(output_df):
-    #     textTag = TextTagConfig(name = feature ,
-    #                             description = feature ,
-    #                             readOnly = True, 
-    #                             arraySize = 0)
-        analogTag = AnalogTagConfig(name = feature,
-                                    description = feature,
-                                    readOnly = False,
-                                    arraySize = 0,
-                                    spanHigh = 100000000000000000000000000000,
-                                    spanLow = -100000000000,
-                                    engineerUnit = '',
-                                    #engineerUnit = None,
-                                    integerDisplayFormat = 30,
-                                    fractionDisplayFormat = 40
-                                    )
 
-        config.node.deviceList[0].analogTagList.append(analogTag)
+    analogTag = AnalogTagConfig(name = 'quantity',
+                                description = 'Production quantity',
+                                readOnly = False,
+                                arraySize = 0,
+                                spanHigh = 10000,
+                                spanLow = 0,
+                                engineerUnit = '',
+                                #engineerUnit = None,
+                                integerDisplayFormat = 30,
+                                fractionDisplayFormat = 40
+                                )
 
-    # TODO: command when config get ready
-    result = edgeAgent.uploadConfig(constant.ActionType['Create'], edgeConfig = config)
-
+    config.node.deviceList[0].analogTagList.append(analogTag)
+    edgeAgent.uploadConfig(constant.ActionType['Create'], edgeConfig = config)
 
     #print(output_df)
     edgeData = EdgeData()
-    for feature in list(output_df):
-        #edgeData = EdgeData()
-        deviceID = device_id
-        tagName = feature
-        #print("===================================")
-        #print(output_df[feature].tail(1).values[0])
-        value = float(output_df[feature].tail(1).values[0])
-        value = "%.2f" % round(value, 2)
-        
-    #         print(value)
-        tag = EdgeTag(deviceID,tagName,value)
-        edgeData.tagList.append(tag)
-        #print("===================================")
-        #print(deviceID,tagName,value)
-        #print("===================================")
-        #print(edgeData)
-        #print(len(str(value)))
-        #print("===================================")
-        #print(tag)
-        # print('scada session #5-1')
-        #result = edgeAgent.sendData(data=edgeData)
-        # print('scada session #5-2')
-        #time.sleep(1)
+
+
+    
+    tag = EdgeTag(DEVICE_ID, 'quantity', data_dict['pline_output'])
+    edgeData.tagList.append(tag)
+    #print("===================================")
+    #print(deviceID,tagName,value)
+    #print("===================================")
+    #print(edgeData)
+    #print(len(str(value)))
+    #print("===================================")
+    #print(tag)
+    # print('scada session #5-1')
+    #result = edgeAgent.sendData(data=edgeData)
+    # print('scada session #5-2')
+    #time.sleep(1)
 
     #     print(datetime.datetime.now(), '|',
     #           inspect.getframeinfo(inspect.currentframe()).function, '| Scada send data')
@@ -128,30 +127,22 @@ def scada_client (data_dict):
 
     #     print('edge datetime', datetime.datetime.fromtimestamp(data_dict['timestamp']) - datetime.timedelta(hours=8))
 
-    print(datetime.datetime.now(), '|',
-            inspect.getframeinfo(inspect.currentframe()).function, '| send data, edge datetime',
-            datetime.datetime.fromtimestamp(data_dict['timestamp']) - datetime.timedelta(hours=8))
 
     result = edgeAgent.sendData(data=edgeData)
 
 
 if __name__ == '__main__':
 
-    SCADA_ID = '18c23d90-9a7a-4168-9a3e-6189db45cd1b'
-    SCADA_NAME = 'kelly'
-
-    device_id = 'm01'
-    device_name = 'machine01'
-
-    edgeAgent = scada_conn (SCADA_ID, device_id, device_name)
-
-    data_dict = {'timestamp': 1638722328,
-                 'device_id': device_id,
-                 'device_name': device_name,
-                 'machine':'machine_01',
-                 'pline_output': 0}
+    # connect
+    edgeAgent = scada_conn ()
+    print(edgeAgent.isConnected())
+    
+    data_dict = dict()
+    data_dict['timestamp'] = 1638722328
+    data_dict['pline_output'] = 1
 
     scada_client(data_dict)
 
-    if edgeAgent is not None: edgeAgent.disconnect()
-    pass
+    # disconnect
+    edgeAgent.disconnect()
+    print(edgeAgent.isConnected())
