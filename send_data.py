804e2f6ec52e60cedcd138a160c1b8271949077e
owen198 <cfleu198@gmail.com>
2021-12-09 12:17:40 +0800

    add

--- send_data.py
+++ send_data.py
@@ -2,6 +2,7 @@
 import datetime
 import time
 import string
+import random
 import threading
 
 from wisepaasdatahubedgesdk.EdgeAgent import EdgeAgent
@@ -12,13 +13,15 @@ from wisepaasdatahubedgesdk.Common.Utils import RepeatedTimer
 NODE_ID = '444cbfad-250c-4243-a14f-5ea956339702'
 DCCS_KEY = "a56a8afb7d6c9595a1d838a6d95c2dlb"
 DCCS_URL = "http://api-dccs-ensaas.aiot.twcc.ai/"
-
+EQU = 'Machine 01'
+VALUE = 10
 
 def on_connected(edgeAgent, isConnected):
     print("connected !")
     config = __generateConfig()
     _edgeAgent.uploadConfig(action = constant.ActionType['Create'], edgeConfig = config)
-    
+  
+  
 def on_disconnected(edgeAgent, isDisconnected):
     print("disconnected !")
 
@@ -29,54 +32,116 @@ def __sendData():
     data = __generateData()
     _edgeAgent.sendData(data)
 
-
+# def __generateBatchData():    # send data in batch for high frequency data
+#   array = []
+#   for n in range(1, 10):
+#     edgeData = EdgeData()
+#     for i in range(1, 1 + 1):
+#       for j in range(1, 1 + 1):
+#         deviceId = 'Device' + str(i)
+#         tagName = 'ATag' + str(j)
+#         value = 2
+#         tag = EdgeTag(deviceId, tagName, value)
+#         edgeData.tagList.append(tag)
+#     #   for j in range(1, 1 + 1):
+#     #     deviceId = 'Device' + str(i)
+#     #     tagName = 'DTag' + str(j)
+#     #     value = random.randint(0,99)
+#     #     value = value % 2
+#     #     tag = EdgeTag(deviceId, tagName, value)
+#     #     edgeData.tagList.append(tag)
+#     #   for j in range(1, 1 + 1):
+#     #     deviceId = 'Device' + str(i)
+#     #     tagName = 'TTag' + str(j)
+#     #     value = random.uniform(0, 100)
+#     #     value = 'TEST ' + str(value)
+#     #     tag = EdgeTag(deviceId, tagName, value)
+#     #     edgeData.tagList.append(tag)
+#     array.append(edgeData)
+#   return array
 
 def __generateData():
     edgeData = EdgeData()
-    deviceId = 'Device1'
-    tagName = 'DTag1'
-    value = 1
-    tag = EdgeTag(deviceId, tagName, value)
-    edgeData.tagList.append(tag)
-
+    for i in range(1, 1 + 1):
+        for j in range(1, 1 + 1):
+            deviceId = EQU
+            tagName = 'quantity'
+            #   value = random.uniform(0, 100)
+            tag = EdgeTag(deviceId, tagName, VALUE)
+            edgeData.tagList.append(tag)
+            # for j in range(1, 1 + 1):
+            #   deviceId = 'Device' + str(i)
+            #   tagName = 'DTag' + str(j)
+            #   value = random.randint(0,99)
+            #   value = value % 2
+            #   tag = EdgeTag(deviceId, tagName, value)
+            #   edgeData.tagList.append(tag)
+            # for j in range(1, 1 + 1):
+            #   deviceId = 'Device' + str(i)
+            #   tagName = 'TTag' + str(j)
+            #   value = random.uniform(0, 100)
+            #   value = 'TEST ' + str(value)
+            #   tag = EdgeTag(deviceId, tagName, value)
+            #   edgeData.tagList.append(tag)
     return edgeData
 
 def __generateConfig():
-  config = EdgeConfig()
-  deviceConfig = DeviceConfig(id = 'Device1',
-                                name = 'Device1',
-                                description = 'Device1',
-                                deviceType = 'Smart Device1',
-                                retentionPolicyName = '')
 
-  discrete = DiscreteTagConfig(name = 'DTag1',
-                                description = 'DTag1',
+    config = EdgeConfig()
+
+    deviceConfig = DeviceConfig(id = EQU,
+                                name = EQU,
+                                description = EQU,
+                                deviceType = 'Gearing Machine',
+                                retentionPolicyName = '')
+    
+    analog = AnalogTagConfig(name = 'quantity',
+                                description = 'quantity',
                                 readOnly = False,
                                 arraySize = 0,
-                                state0 = 'Stop',
-                                state1 = 'Start')
+                                spanHigh = 1000,
+                                spanLow = 0,
+                                engineerUnit = '',
+                                integerDisplayFormat = 4,
+                                fractionDisplayFormat = 2)
 
-  deviceConfig.discreteTagList.append(discrete)
+    deviceConfig.analogTagList.append(analog)
 
-  config.node.deviceList.append(deviceConfig)
-  return config
+#   discrete = DiscreteTagConfig(name = 'DTag1',
+#     description = 'DTag1',
+#     readOnly = False,
+#     arraySize = 0,
+#     state0 = 'Stop',
+#     state1 = 'Start')
+#   deviceConfig.discreteTagList.append(discrete)
+  
+#   text = TextTagConfig(name = 'TTag1',
+#     description = 'TTag1',
+#     readOnly = False,
+#     arraySize = 0)
+#   deviceConfig.textTagList.append(text)
 
-if __name__ == '__main__':
+    config.node.deviceList.append(deviceConfig)
+    return config
 
-    _edgeAgent = None
-    edgeAgentOptions = EdgeAgentOptions(nodeId = NODE_ID)
-    edgeAgentOptions.connectType = constant.ConnectType['DCCS']
-    dccsOptions = DCCSOptions(apiUrl = DCCS_URL, credentialKey = DCCS_KEY)
-    edgeAgentOptions.DCCS = dccsOptions
-    _edgeAgent = EdgeAgent(edgeAgentOptions)
-    _edgeAgent.on_connected = on_connected
-    _edgeAgent.on_disconnected = on_disconnected
-    _edgeAgent.on_message = edgeAgent_on_message
 
-    _edgeAgent.connect()
+_edgeAgent = None
+edgeAgentOptions = EdgeAgentOptions(nodeId = NODE_ID)
+edgeAgentOptions.connectType = constant.ConnectType['DCCS']
+dccsOptions = DCCSOptions(apiUrl = DCCS_URL, credentialKey = DCCS_KEY)
+edgeAgentOptions.DCCS = dccsOptions
+_edgeAgent = EdgeAgent(edgeAgentOptions)
+_edgeAgent.on_connected = on_connected
+_edgeAgent.on_disconnected = on_disconnected
+_edgeAgent.on_message = edgeAgent_on_message
 
-    time.sleep(5)  # Waiting for connection to be established
+_edgeAgent.connect()
 
-    __sendData()
-    time.sleep(1)
-    _edgeAgent.disconnect()
+time.sleep(5)  # Waiting for connection to be established
+
+__sendData()
+time.sleep(1)
+
+# for i in range(1, 2):
+    # __sendData()
+    # time.sleep(1)
