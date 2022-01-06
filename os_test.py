085c84546feb7922b23d0da64a4f428a32eb6b1d
BlackLutos <gcobs172250@gmail.com>
2022-01-06 10:44:47 +0800

    Delete os_test.py

--- os_test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system('START /B python send_data.py 03')
-print('1')
-os.system('START /B python send_data.py 03')
-print('2')
-os.system('START /B python send_data.py 03')
-print('3')
